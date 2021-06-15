8b5aa16feeb739d016e5713d2b57450c17b757fb
Michael Jepsen <mdj@systemate.dk>
2021-06-15 13:04:00 +0200

    Add message receiver HTTP trigger (#78)
    * Add noda time to domain
    
    * Add correlation ID handling from messaging
    
    * Add MessageReceiver project with HTTP trigger
    
    * Add MessageReceiver to solution

---
+++ source/GreenEnergyHub.TimeSeries/source/GreenEnergyHub.TimeSeries.MessageReceiver/TimeSeriesHttpTrigger.cs
@@ -0,0 +1,59 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using GreenEnergyHub.TimeSeries.Infrastructure.Messaging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace GreenEnergyHub.TimeSeries.MessageReceiver
+{
+    public class TimeSeriesHttpTrigger
+    {
+        /// <summary>
+        /// The name of the function.
+        /// Function name affects the URL and thus possibly dependent infrastructure.
+        /// </summary>
+        private const string FunctionName = "TimeSeriesHttpTrigger";
+        private readonly ICorrelationContext _correlationContext;
+
+        public TimeSeriesHttpTrigger(ICorrelationContext correlationContext)
+        {
+            _correlationContext = correlationContext;
+        }
+
+        [FunctionName(FunctionName)]
+        public async Task<IActionResult> RunAsync(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]
+            [NotNull] HttpRequest req,
+            [NotNull] ExecutionContext context,
+            ILogger log)
+        {
+            log.LogInformation("Function {FunctionName} started to process a request with a body of size {SizeOfBody}", FunctionName, req.Body.Length);
+
+            SetupCorrelationContext(context);
+
+            return await Task.FromResult(new OkResult()).ConfigureAwait(false);
+        }
+
+        private void SetupCorrelationContext(ExecutionContext context)
+        {
+            _correlationContext.CorrelationId = context.InvocationId.ToString();
+        }
+    }
+}
