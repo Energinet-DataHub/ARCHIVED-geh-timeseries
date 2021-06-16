fd8d2b5f8a063dbcf947c8acd18ef4f3d1a3ba9e
Michael Dalgaard Jepsen <mdj@systemate.dk>
2021-06-16 11:36:44 +0200

    Add time series representation

---
+++ source/GreenEnergyHub.TimeSeries/source/GreenEnergyHub.TimeSeries.Domain/Notification/Transaction/Series.cs
@@ -0,0 +1,42 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using NodaTime;
+
+#pragma warning disable 8618
+
+namespace GreenEnergyHub.TimeSeries.Domain.Notification.Transaction
+{
+    public class Series
+    {
+        /// <summary>
+        /// Contains a unique ID for the specific times series, provided by the sender.
+        /// </summary>
+        public string Id { get; set; }
+
+        public Product Product { get; set; }
+
+        public string MeteringPointId { get; set; }
+
+        public MeteringPointType MeteringPointType { get; set; }
+
+        public SettlementMethod? SettlementMethod { get; set; }
+
+        public Instant RegistrationDateTime { get; set; }
+
+        public MeasureUnit Unit { get; set; }
+
+        public SeriesPeriod Period { get; set; }
+    }
+}
