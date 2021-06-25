c88dc35906da5deec20888ba31b97de5edd40dd9
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2021-06-25 06:46:08 +0000

    Copied from archived repo

---
+++ source/streaming/integration-tests/helpers/spark_helper.py
@@ -0,0 +1,75 @@
+# Copyright 2020 Energinet DataHub A/S
+#
+# Licensed under the Apache License, Version 2.0 (the "License2");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import array, array_except, when, col, lit
+from pyspark.sql.utils import AnalysisException
+import time
+import datetime
+from datetime import timedelta
+
+
+def get_base_storage_path(storage_container_name, storage_account_name):
+    return "abfss://{0}@{1}.dfs.core.windows.net/".format(
+        storage_container_name, storage_account_name)
+
+
+def get_spark_session(storage_account_name, storage_account_key):
+    spark_conf = SparkConf(loadDefaults=True) \
+        .set('fs.azure.account.key.{0}.dfs.core.windows.net'.format(storage_account_name), storage_account_key)
+
+    spark = spark = SparkSession\
+        .builder\
+        .config(conf=spark_conf)\
+        .getOrCreate()
+
+    sc = spark.sparkContext
+    print("Spark Configuration:")
+    _ = [print(k + '=' + v) for k, v in sc.getConf().getAll()]
+
+    return spark
+
+
+def find_rows_in_delta_lake(spark: SparkSession, directory, column_name, column_value):
+    # Handle exception that occurs if necessary Delta directory hasn't been created by the streaming yet
+    # and no other or previous test runs have created it.
+    try:
+        df = spark.read.format("delta").load(directory)
+    except AnalysisException:
+        return None
+
+    filter_result = df.filter(col(column_name) == column_value)
+
+    if filter_result.count() == 0:
+        return None
+
+    return filter_result.collect()
+
+
+def get_stored_value_in_deltalake(spark, function_timeout_minutes, directory, column_name, column_value):
+    sleep_time_in_seconds = 1
+    wait_until = datetime.datetime.now() + timedelta(minutes=function_timeout_minutes)
+    break_loop = False
+    while not break_loop:
+        print("Checking Delta Lake for expected time series points...")
+        rows = find_rows_in_delta_lake(spark, directory, column_name, column_value)
+
+        if rows is not None:
+            return rows
+
+        time.sleep(sleep_time_in_seconds)
+        if wait_until < datetime.datetime.now():
+            break_loop = True
+
+    raise Exception("Time out while looking/waiting for data in Delta Lake %s with '%s'='%s'." % (directory, column_name, column_value))
---
+++ source/streaming/integration-tests/test_streaming.py
@@ -0,0 +1,151 @@
+# Copyright 2020 Energinet DataHub A/S
+#
+# Licensed under the Apache License, Version 2.0 (the "License2");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import pytest
+import sys
+import asyncio
+import uuid
+from helpers import spark_helper, eventhub_helper, file_reader
+
+"""
+HOW TO RUN THE TEST(S):
+To run it execute the test 5 arguments need to be submitted when executing
+
+storage_account_name = sys.argv[1]
+storage_account_key = sys.argv[2]
+storage_container_name = sys.argv[3]
+input_eh_connection_string = sys.argv[4]
+delta_lake_output_path = sys.argv[5]
+
+Example: python streaming-test.py storage_account_name storage_account_key storage_container_name "input_eh_connection_string" "delta/meter-data"
+
+Remember to put "" around input_eh_connection_string
+
+In addition, the streaming and validation job must be running. If running manually you can execute the following
+command from /src/streaming in a new terminal:
+
+    python enrichment_and_validation.py
+"""
+
+TIMEOUT_IN_MINUTES = 1
+CORRELATION_ID_IN_FILE = "WILL BE REPLACED BY INTEGRATION TEST"
+
+sys.argv = [
+    sys.argv[0],
+    "",
+    "",
+    "messagedata",
+    "",
+    "delta/meter-data"
+]
+
+storage_account_name = sys.argv[1]
+storage_account_key = sys.argv[2]
+storage_container_name = sys.argv[3]
+input_eh_connection_string = sys.argv[4]
+delta_lake_output_path = sys.argv[5]
+
+spark = spark_helper.get_spark_session(storage_account_name, storage_account_key)
+delta_lake_base_path = spark_helper.get_base_storage_path(storage_container_name, storage_account_name)
+
+import datetime
+from decimal import Decimal
+import pathlib
+script_dir = str(pathlib.Path(__file__).parent.absolute())
+consumption_time_series_message_json_path = script_dir + "/test-files/valid_time_series_message.json"
+exchange_time_series_message_json_path = script_dir + "/test-files/valid_time_series_message_for_exchange.json"
+
+
+def __get_time_series_from_delta_lake_by_correlation_id(correlation_id):
+    return spark_helper.get_stored_value_in_deltalake(
+        spark,
+        TIMEOUT_IN_MINUTES,
+        delta_lake_base_path + delta_lake_output_path,
+        "correlationId",
+        correlation_id)
+
+
+async def test_streaming_adds_valid_time_series_with_all_required_data_to_delta_lake():
+    """
+    This is an integration test, which tests that the streaming job stores all required streamed and enriched fields
+    of valid time series in Delta Lake for aggregations.
+
+    It is worth to note that test sends to different messages (one for at consumption market evalution point and one
+    for an exchange point) in order to observe values in all fields.
+
+    Please don't update this test without ensuring that necessary changes to Delta Lake data format has
+    also been made.
+    """
+
+    # Arrange
+    consumption_time_series_template = file_reader.read_file_as_string(consumption_time_series_message_json_path)
+    consumption_correlation_id = str(uuid.uuid4())  # Make sure correlation ID is unique to avoid interference with others
+    consumption_time_series_message = consumption_time_series_template.replace(CORRELATION_ID_IN_FILE, consumption_correlation_id)
+
+    exchange_time_series_template = file_reader.read_file_as_string(exchange_time_series_message_json_path)
+    exchange_correlation_id = str(uuid.uuid4())  # Make sure correlation ID is unique to avoid interference with others
+    exchange_time_series_message = exchange_time_series_template.replace(CORRELATION_ID_IN_FILE, exchange_correlation_id)
+
+    # Act
+    await eventhub_helper.insert_content_on_eventhub(input_eh_connection_string, consumption_time_series_message)
+    await eventhub_helper.insert_content_on_eventhub(input_eh_connection_string, exchange_time_series_message)
+
+    # Assert
+    consumption_time_series_points = __get_time_series_from_delta_lake_by_correlation_id(consumption_correlation_id)
+
+    point = next(p for p in consumption_time_series_points if p.series_observationDateTime == datetime.datetime(2020, 11, 13, 1, 0))
+
+    assert point.MarketEvaluationPoint_mRID == "571313180000000005"
+    assert point.Time == datetime.datetime(2020, 11, 13, 1, 0)
+    assert point.Quantity == Decimal("3.337")
+    assert point.CorrelationId == consumption_correlation_id
+    assert point.MessageReference == "mId2020-12-01T13:16:29.330Z"
+    assert point.MarketDocument_mRID == "hId2020-12-01T13:16:29.330Z"
+    assert point.CreatedDateTime == datetime.datetime(2020, 12, 1, 13, 16, 29, 330000)
+    assert point.SenderMarketParticipant_mRID == "8100000000030"
+    assert point.ProcessType == "D42"
+    assert point.SenderMarketParticipantMarketRole_Type == "MDR"
+    assert point.TimeSeries_mRID == "tId2020-12-01T13:16:29.330Z"
+    assert point.MktActivityRecord_Status == "9"
+    assert point.MarketEvaluationPointType == "E17"
+    assert point.Quality == "56"
+    assert point.MeterReadingPeriodicity == "PT1H"
+    assert point.MeteringMethod == "D01"
+    assert point.MeteringGridArea_Domain_mRID == "800"
+    assert point.ConnectionState == "E22"
+    assert point.EnergySupplier_MarketParticipant_mRID == "8100000000108"
+    assert point.BalanceResponsibleParty_MarketParticipant_mRID == "8100000000207"
+    assert point.InMeteringGridArea_Domain_mRID is None
+    assert point.OutMeteringGridArea_Domain_mRID is None
+    assert point.Parent_Domain_mRID is None
+    assert point.ServiceCategory_Kind == "23"
+    assert point.SettlementMethod == "D01"
+    assert point.QuantityMeasurementUnit_Name == "KWH"
+    assert point.Product == "8716867000030"
+    assert point.year == 2020
+    assert point.month == 11
+    assert point.day == 13
+
+    exchange_time_series_points = __get_time_series_from_delta_lake_by_correlation_id(exchange_correlation_id)
+    exchange_point = next(p for p in exchange_time_series_points if p.Time == datetime.datetime(2020, 12, 6, 0, 0))
+
+    # These are the only fields we couldn't test with the consumption message - no need to assert the rest of the fields again.
+    assert exchange_point.InMeteringGridArea_Domain_mRID == "802"
+    assert exchange_point.OutMeteringGridArea_Domain_mRID == "803"
+    assert exchange_point.Parent_Domain_mRID == "571313180000000074"
+
+
+# Eventhub requires async https://docs.microsoft.com/en-us/azure/event-hubs/event-hubs-python-get-started-send#send-events
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test_streaming_adds_valid_time_series_with_all_required_data_to_delta_lake())
+loop.close()
