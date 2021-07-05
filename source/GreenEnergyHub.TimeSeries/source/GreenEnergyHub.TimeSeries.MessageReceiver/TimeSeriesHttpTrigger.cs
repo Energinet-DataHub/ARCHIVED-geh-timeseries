7cf1db6b134f456e9fcf21f6a560ed3fb038f766
Lasse Ringgren Nielsen <lrn@endk.dk>
2021-07-05 13:10:19 +0200

    reverted tested changes

--- source/GreenEnergyHub.TimeSeries/source/GreenEnergyHub.TimeSeries.MessageReceiver/TimeSeriesHttpTrigger.cs
+++ source/GreenEnergyHub.TimeSeries/source/GreenEnergyHub.TimeSeries.MessageReceiver/TimeSeriesHttpTrigger.cs
@@ -31,7 +31,6 @@ namespace GreenEnergyHub.TimeSeries.MessageReceiver
         /// <summary>
         /// The name of the function.
         /// Function name affects the URL and thus possibly dependent infrastructure.
-        /// Newline just for fun.
         /// </summary>
         private const string FunctionName = "TimeSeriesHttpTrigger";
         private readonly ICorrelationContext _correlationContext;
