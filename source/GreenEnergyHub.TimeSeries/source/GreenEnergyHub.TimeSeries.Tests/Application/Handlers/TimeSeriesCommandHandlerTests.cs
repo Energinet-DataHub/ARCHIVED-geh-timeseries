009fcf306a53b3957dfb0947789083760fd6a93d
Michael Dalgaard Jepsen <mdj@systemate.dk>
2021-07-12 08:04:45 +0200

    Add test of TimeSeriesCommandHandler

---
+++ source/GreenEnergyHub.TimeSeries/source/GreenEnergyHub.TimeSeries.Tests/Application/Handlers/TimeSeriesCommandHandlerTests.cs
@@ -0,0 +1,59 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.Xunit2;
+using GreenEnergyHub.TimeSeries.Application;
+using GreenEnergyHub.TimeSeries.Application.Handlers;
+using GreenEnergyHub.TimeSeries.Domain.Notification;
+using GreenEnergyHub.TimeSeries.Infrastructure.Messaging;
+using GreenEnergyHub.TimeSeries.TestCore;
+using Moq;
+using Xunit;
+using Xunit.Categories;
+
+namespace GreenEnergyHub.TimeSeries.Tests.Application.Handlers
+{
+    [UnitTest]
+    public class TimeSeriesCommandHandlerTests
+    {
+        [Theory]
+        [InlineAutoMoqData]
+        public async Task HandleAsync_WhenCalled_DispatchesCommandAndReturnsSuccessfulResult(
+            [Frozen] [NotNull] Mock<IMessageDispatcher<TimeSeriesCommand>> dispatcher,
+            [NotNull] TimeSeriesCommand command,
+            [NotNull] TimeSeriesCommandHandler sut)
+        {
+            // Arrange
+            var dispatched = false;
+            dispatcher.Setup(
+                    d => d.DispatchAsync(
+                        command,
+                        It.IsAny<CancellationToken>()))
+                .Callback<TimeSeriesCommand, CancellationToken>(
+                    (_, _) => dispatched = true);
+
+            // Act
+            var result = await sut.HandleAsync(command).ConfigureAwait(false);
+
+            // Assert
+            Assert.True(dispatched);
+            Assert.NotNull(result);
+            Assert.True(result.IsSucceeded);
+        }
+    }
+}
