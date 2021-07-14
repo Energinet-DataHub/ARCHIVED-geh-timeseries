3df78cb83282cffe925686c8b282dbb990169fa3
Lasse Nielsen <lrn@energinet.dk>
2021-07-14 14:20:41 +0200

    timeseries protobuf file created on pip install

--- source/streaming/setup.py
+++ source/streaming/setup.py
@@ -13,6 +13,35 @@
 # limitations under the License.
 from setuptools import setup, find_packages
 
+import pathlib
+import os
+from subprocess import check_call
+
+def generate_proto_code():
+    proto_interface_dir = "./contracts/"
+    generated_src_dir = "./generated/"
+    out_folder = "src"
+    if not os.path.exists(generated_src_dir):
+        os.mkdir(generated_src_dir)
+    proto_it = pathlib.Path().glob(proto_interface_dir + "/**/*")
+    proto_path = "generated=" + proto_interface_dir
+    protos = [str(proto) for proto in proto_it if proto.is_file()]
+    check_call(["protoc"] + protos + ["--python_out", out_folder, "--proto_path", proto_path])
+
+from setuptools.command.develop import develop
+from setuptools import setup, find_packages
+
+class CustomDevelopCommand(develop):
+    """Wrapper for custom commands to run before package installation."""
+    uninstall = False
+
+    def run(self):
+        develop.run(self)
+
+    def install_for_development(self):
+        develop.install_for_development(self)
+        generate_proto_code()
+
 # File 'VERSION' is created by pipeline. If executed manual it must be created manually.
 __version__ = ""
 with open('VERSION') as version_file:
@@ -24,5 +53,8 @@ setup(name='geh_stream',
       long_description="",
       long_description_content_type='text/markdown',
       license='MIT',
-      packages=find_packages()
+      packages=find_packages(),
+      cmdclass={
+        'develop': CustomDevelopCommand, # used for pip install -e ./
+       },
       )
--- source/streaming/setup.sh
+++ source/streaming/setup.sh
@@ -3,4 +3,5 @@
 python -m pip install flake8
 python setup.py install
 python setup.py sdist bdist_wheel
+python pip install -e ./
 bash debugz.sh
