5d3cf263ffb17193cadcbad7246f72359a3f9c17
HenrikSommer <54191286+HenrikSommer@users.noreply.github.com>
2021-09-28 22:49:41 +0200

    Remove presumably unnecessary python class generation

--- .github/actions/databricks-unit-test/entrypoint.sh
+++ .github/actions/databricks-unit-test/entrypoint.sh
@@ -2,8 +2,6 @@
 
 cd ./source/streaming
 python setup.py install
-# Generate Python classes from ProtoBuf contracts
-python -m pip install -e ./
 # python coverage-threshold install
 pip install coverage-threshold
 coverage run --branch -m pytest tests/
