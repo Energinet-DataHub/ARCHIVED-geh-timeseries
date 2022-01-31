976f330f59ea68da8016173cf3dc9816a8d73295
Lasse Klitgaard <lki@energinet.dk>
2022-01-31 15:22:52 +0100

    remove unused keyvault secrets

--- build/primary/main/kv-shared-resources.tf
+++ build/primary/main/kv-shared-resources.tf
@@ -16,26 +16,6 @@ data "azurerm_key_vault" "kv_shared_resources" {
   resource_group_name = var.shared_resources_resource_group_name
 }
 
-data "azurerm_key_vault_secret" "sql_data_name" {
-  name         = "sql-data-name"
-  key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
-}
-
-data "azurerm_key_vault_secret" "sql_data_admin_name" {
-  name         = "sql-data-admin-user-name"
-  key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
-}
-
-data "azurerm_key_vault_secret" "sql_data_admin_password" {
-  name         = "sql-data-admin-user-password"
-  key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
-}
-
-data "azurerm_key_vault_secret" "sql_data_url" {
-  name         = "sql-data-url"
-  key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
-}
-
 data "azurerm_key_vault_secret" "sb_domain_relay_listener_connection_string" {
   name         = "sb-domain-relay-listen-connection-string"
   key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
