25e6f5d6c273eb247bad2227c4dcf26791024bdc
Kristian Schneider <khs@thamesconsulting.com>
2022-02-11 10:02:47 +0100

    cluster name

--- build/databricks_streaming_cluster/main/main.tf
+++ build/databricks_streaming_cluster/main/main.tf
@@ -18,7 +18,8 @@ resource "databricks_job" "streaming_job" {
   max_concurrent_runs = 1   
   always_running = true
 
-  new_cluster { 
+  new_cluster {
+    cluster_name            = "Timeseries-cluster"
     spark_version           = "8.4.x-scala2.12"
     node_type_id            = "Standard_DS3_v2"
     num_workers    = 1
