46ac31afcc05b5980f55c12a73afd15d3aff6740
Lasse Nielsen <lrn@energinet.dk>
2022-02-17 14:18:54 +0100

    updated name

--- build/primary/main/func-time-series-bundle-ingestor.tf
+++ build/primary/main/func-time-series-bundle-ingestor.tf
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-module "func_message_recevier" {
+module "time_series_bundle_ingestor" {
   source                                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/function-app?ref=5.1.0"
 
   name                                      = "time-series-bundle-ingestor"
