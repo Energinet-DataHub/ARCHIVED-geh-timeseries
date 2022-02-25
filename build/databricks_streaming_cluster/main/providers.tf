cd64e3d418270cf719f301ba259cd9b089a391d1
Kristian Schneider <khs@thamesconsulting.com>
2022-02-25 13:56:03 +0100

    attempt to fix pipeline

--- build/databricks_streaming_cluster/main/providers.tf
+++ build/databricks_streaming_cluster/main/providers.tf
@@ -25,6 +25,7 @@ terraform {
 
 provider "databricks" {
   azure_workspace_resource_id = data.azurerm_key_vault_secret.dbw_databricks_workspace_id.value
+  host = data.azurerm_key_vault_secret.dbw_databricks_workspace_url.value
 }
 
 provider "azurerm" {
