de60ba504b2111b0825f30a5238f5d0c59bbe96b
Lasse Klitgaard <lki@energinet.dk>
2022-02-25 10:16:08 +0100

    bump databricks provider

--- build/databricks_streaming_cluster/main/providers.tf
+++ build/databricks_streaming_cluster/main/providers.tf
@@ -17,7 +17,7 @@ terraform {
   required_providers {
     databricks = {
       source = "databrickslabs/databricks"
-      version = "0.3.7"
+      version = "0.5.1"
     }
     azurerm = "=2.78.0"
   }
