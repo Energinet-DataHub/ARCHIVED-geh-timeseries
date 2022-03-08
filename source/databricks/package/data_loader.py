a5328eaec4e35593d81a9ceb7b9dee7a4eed7e90
Lasse Klitgaard <lki@energinet.dk>
2022-03-08 11:43:48 +0100

    add schema evolution to spark configuration

--- source/databricks/package/data_loader.py
+++ source/databricks/package/data_loader.py
@@ -23,7 +23,8 @@ def initialize_spark(args):
         .set(f'fs.azure.account.key.{args.data_storage_account_name}.dfs.core.windows.net', args.data_storage_account_key) \
         .set('spark.sql.session.timeZone', 'UTC') \
         .set('spark.databricks.io.cache.enabled', 'True') \
-        .set('spark.databricks.delta.formatCheck.enabled', 'False')
+        .set('spark.databricks.delta.formatCheck.enabled', 'False') \
+        .set('spark.databricks.delta.schema.autoMerge.enabled', 'True')
 
     if args_dict.get('shared_storage_account_name') is not None:
         spark_conf.set(f'fs.azure.account.key.{args.shared_storage_account_name}.dfs.core.windows.net', args.shared_storage_account_key)
