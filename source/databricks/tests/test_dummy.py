961dd615869cc8b3c13710685946a4aef1ef8269
BjarkeMeier <hej@bjarkemeier.dk>
2022-03-18 09:15:09 +0000

    Demo spark job termination and more cool stuff

--- source/databricks/tests/test_dummy.py
+++ source/databricks/tests/test_dummy.py
@@ -1,5 +1,8 @@
+import asyncio
 import pytest
 import os
+import threading
+import time
 from pyspark import SparkConf
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.functions import col, lit, to_timestamp, explode
@@ -9,13 +12,38 @@ from pyspark.sql.types import StructType, StructField, StringType, ArrayType, \
 
 @pytest.fixture()
 def spark():
+    # spark.hadoop.fs.* for Azurite storage
     spark_conf = SparkConf(loadDefaults=True) \
         .set("spark.sql.session.timeZone", "UTC")
+        #.set("spark.hadoop.fs.defaultFS", "wasb://container@azurite") \
+        #.set("spark.hadoop.fs.azure.storage.emulator.account.name", "azurite")
     return SparkSession \
         .builder \
         .config(conf=spark_conf) \
         .config("spark.sql.streaming.schemaInference", True) \
         .getOrCreate()
+
+
+class Task(threading.Thread):
+    def __init__(self, func):
+        threading.Thread.__init__(self)
+        self.func = func
+
+    def run(self):
+        """Target function of the thread class"""
+        self.function()
+
+
+@pytest.fixture(scope="session")
+def azurite():
+    import subprocess
+    azurite_process = subprocess.Popen(args=["azurite-blob", "-l", ".azurite-files"], stdout=subprocess.PIPE)
+
+    # import os
+    # os.system("azurite-blob -l .azurite-files")
+    yield
+    azurite_process.terminate()
+    print("############ Azurite has terminated")
 
 
 """
@@ -24,10 +52,8 @@ schema = StructType(
                  StructField("first_name", StringType, true),
                  StructField("last_name", StringType, true))])
 """
-@pytest.fixture
-def my_job(spark: SparkSession):
-    print(os.getcwd())
 
+def spark_job(spark):
     raw_stream =(spark
             .readStream
             #.schema(schema)
@@ -39,9 +65,54 @@ def my_job(spark: SparkSession):
         .format("console")
         .start())
 
-    #query.awaitTermination()
+    # query.awaitTermination()
     return query
 
 
-def test_my_job(my_job):
+@pytest.fixture
+def my_job_function(spark: SparkSession, azurite):
+    return lambda: spark_job(spark)
+
+"""
+def test_spark(my_job_function):
+    my_job_function().awaitTermination()
+    print("############ Never get here")
+"""
+
+"""
+def test_my_job(my_job_function):
+    task = Task(my_job_function)
+    task.start()
+    time.sleep(5)
+    task.join()
     my_job.awaitTermination()
+"""
+
+
+async def job_task(spark):
+    raw_stream =(spark
+        .readStream
+        #.schema(schema)
+        .json("/workspaces/geh-timeseries/source/databricks/tests/test_data*.json"))
+
+    query = (raw_stream
+        .writeStream
+        .outputMode("append")
+        .format("console")
+        .start())
+    try:
+        print("############# About to await termination")
+        quey.awaitTermination()
+    except asyncio.CancelledError:
+        print("############# About to stop spark job")
+        query.stop()
+        raise
+
+
+@pytest.mark.asyncio
+async def test_cancel(spark, azurite):
+    task = asyncio.create_task(job_task(spark))
+    #await task
+    await asyncio.sleep(10)
+    task.cancel()
+    print("Done")
