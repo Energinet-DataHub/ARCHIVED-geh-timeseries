8f594c1be48a15949ca857241aa5b2a03a9f0f86
Kristian Schneider <khs@thamesconsulting.com>
2022-03-21 10:45:59 +0100

    Timeseries ETL (#405)
    * Initial approach from timeseries ETL
    
    * .
    
    * Initial first unit test with schema validation
    
    * reintroduce dbr CI
    
    * lint
    
    * trigger pipeline
    
    * -
    
    * use 5.0.2
    
    * .
    
    * More unit tests and feedback from PR
    
    * lint
    
    * bump databricks provider
    
    * bump terraform provider
    
    * Update providers.tf
    
    * Update providers.tf
    
    * Update ci.yml
    
    * attempt to fix pipeline
    
    * .
    
    * .
    
    * Update providers.tf
    
    * add year, month and day
    
    * Prepare for extra cluster and job
    
    * flake
    
    * Conditional build
    
    * .
    
    * .
    
    * .
    
    * refactor jsonTransformer
    
    * add columns to timeseries schema
    
    * incomment all columns in eventhub time series schema
    
    * refactor jsonTransformer to add interval to start date time
    
    * add schema evolution to spark configuration
    
    * flake8 fixes
    
    * check dataframe has content
    
    * flake8 fix
    
    * new up transformer before use
    
    * instantiate jsonTransformer
    
    * add unit test for interval addition after explode
    
    * add insert and merge functionality
    
    * .
    
    * test and docker adjustment
    
    * .
    
    * .
    
    * .
    
    * .
    
    * add min and max functions
    
    * .
    
    * .
    
    * .
    
    * use truthy condition
    
    * use string value to filter on boolean column
    
    * working container with delta table support
    
    * reintroduce steps
    
    * more python tools
    
    * select year, month, day and timeseries when persisting eventhub item
    
    * add general documentation to function
    
    * Update TimeSeriesBundleDtoValidatingDeserializerTests.cs
    
    * change name of streaming job
    
    * cast column to string
    
    * don't use col
    
    * remove awaitTermination from streaming jobs
    
    * multiline strings
    
    Co-authored-by: Lasse Klitgaard <lki@energinet.dk>
    Co-authored-by: René T. Nielsen <renetdev@gmail.com>

--- source/databricks/package/codelists/resolution.py
+++ source/databricks/package/codelists/resolution.py
@@ -11,3 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
+class Resolution():
+    quarter = "1"
+    hour = "2"
+    day = "3"
+    month = "4"
