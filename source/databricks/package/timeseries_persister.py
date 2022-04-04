887efaa9768c7eb7e0814c4cf16d7061137a0b70
Kristian Schneider <khs@thamesconsulting.com>
2022-04-04 10:04:37 +0200

    trigger pipeline

--- source/databricks/package/timeseries_persister.py
+++ source/databricks/package/timeseries_persister.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import StringType
 from pyspark.sql.functions import year, month, dayofmonth
