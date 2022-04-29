d31ee7d06f23c2d8ec51e3430c6a7df76185c3f6
Dan Stenrøjl <51327761+dstsyst@users.noreply.github.com>
2022-04-29 07:45:51 +0200

    Upgrade to .NET 6 and Functions v4 (#461)
    * Upgrade TargetFramework and AzureFunctionsVersion
    
    * Update path to net6.0
    
    * Remove deprecated Analyzers.
    Use new Analyzers and add them as development references.
    
    * Added section headers
    
    * Added IDE0079
    
    * Consolidate NuGet package version
    
    * Update CI build
    
    * Update CD pipeline
    
    * Update key name
    
    * Update TF function
    
    * Use Python workflow 7.0.0 Copied the old shared action into domain.
    
    * Update Python test and coverage to 7.0.0 for CD pipeline

--- build/primary/main/func-time-series-bundle-ingestor.tf
+++ build/primary/main/func-time-series-bundle-ingestor.tf
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 module "time_series_bundle_ingestor" {
-  source                                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/function-app?ref=5.8.0"
+  source                                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/function-app?ref=5.12.0"
 
   name                                      = "time-series-bundle-ingestor"
   project_name                              = var.domain_name_short
@@ -21,7 +21,8 @@ module "time_series_bundle_ingestor" {
   resource_group_name                       = azurerm_resource_group.this.name
   location                                  = azurerm_resource_group.this.location
   app_service_plan_id                       = data.azurerm_key_vault_secret.plan_shared_id.value
-  application_insights_instrumentation_key  = data.azurerm_key_vault_secret.appi_instrumentation_key.value
+  application_insights_instrumentation_key  = data.azurerm_key_vault_secret.appi_shared_instrumentation_key.value
+  log_analytics_workspace_id                = data.azurerm_key_vault_secret.log_shared_id.value
   always_on                                 = true
   health_check_path                         = "/api/monitor/ready"
   health_check_alert_action_group_id        = data.azurerm_key_vault_secret.primary_action_group_id.value
@@ -40,6 +41,6 @@ module "time_series_bundle_ingestor" {
     EVENT_HUB_CONNECTION_STRING                         = module.evh_received_timeseries.primary_connection_strings["send"]
     EVENT_HUB_NAME                                      = module.evh_received_timeseries.name
   }
-  
+
   tags                                      = azurerm_resource_group.this.tags
 }
