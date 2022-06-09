0db3fddbe32132cd3e0a1c9e8a79b271ef60de03
seblapela <102210697+seblapela@users.noreply.github.com>
2022-06-09 09:23:32 +0200

    fix checkpoint path and add timeseries publisher integration test (#465)
    * fix checkpoint path and add timeseries publisher integration test
    * Use the Dockerfile of the domain
    * Fix GitHub action
    * Make entrypoint.sh executable
    
    Co-authored-by: sebastian <sebastian.roerup@persgroep.net>
    Co-authored-by: BjarkeMeier <hej@bjarkemeier.dk>

--- build/databricks_streaming_cluster/main/main.tf
+++ build/databricks_streaming_cluster/main/main.tf
@@ -56,8 +56,9 @@ resource "databricks_job" "persister_streaming_job" {
          "--data-storage-account-name=${data.azurerm_key_vault_secret.st_data_lake_name.value}",
          "--data-storage-account-key=${data.azurerm_key_vault_secret.st_data_lake_primary_access_key.value}",
          "--event-hub-connection-key=${var.evh_timeseries_listen_connection_string}",
-         "--delta-lake-container-name=timeseries-data",
-         "--timeseries-unprocessed-blob-name=timeseries-unprocessed",
+         "--time_series_unprocessed_path=abfss://${timeseries-data}@${data.azurerm_key_vault_secret.st_data_lake_name.value}.dfs.core.windows.net/timeseries-unprocessed",
+         "--time_series_checkpoint_path=abfss://${timeseries-data}@${data.azurerm_key_vault_secret.st_data_lake_name.value}.dfs.core.windows.net/checkpoint-timeseries-persister"
+
     ]
   }
 
@@ -108,9 +109,9 @@ resource "databricks_job" "publisher_streaming_job" {
          "--data-storage-account-name=${data.azurerm_key_vault_secret.st_data_lake_name.value}",
          "--data-storage-account-key=${data.azurerm_key_vault_secret.st_data_lake_primary_access_key.value}",
          "--event-hub-connection-key=${var.evh_timeseries_listen_connection_string}",
-         "--delta-lake-container-name=timeseries-data",
-         "--timeseries-unprocessed-blob-name=timeseries-unprocessed",
-         "--time-series-points-blob-name=time-series-points"
+         "--time_series_unprocessed_path=abfss://${timeseries-data}@${data.azurerm_key_vault_secret.st_data_lake_name.value}.dfs.core.windows.net/timeseries-unprocessed",
+         "--time_series_points_path=abfss://${timeseries-data}@${data.azurerm_key_vault_secret.st_data_lake_name.value}.dfs.core.windows.net/time-series-points",
+         "--time_series_checkpoint_path=abfss://${timeseries-data}@${data.azurerm_key_vault_secret.st_data_lake_name.value}.dfs.core.windows.net/checkpoint-timeseries-publisher"
     ]
   }
 
