5653d241ae9f7ad97b4e4267f9b334fa3159f56d
Jonas Michelsen <jvm@energinet.dk>
2022-06-17 08:45:50 +0200

    Revert "Update TimeSeriesBundleIngestionEndpointTests.cs"
    This reverts commit 9a2d34e5a4f7f1a2f5596bf57ada162dd50eefe4.

--- source/TimeSeries/IntegrationTests/TimeSeriesBundleIngestionEndpointTests.cs
+++ source/TimeSeries/IntegrationTests/TimeSeriesBundleIngestionEndpointTests.cs
@@ -96,6 +96,52 @@ namespace Energinet.DataHub.TimeSeries.IntegrationTests
             twoNewest.Should().Contain(x => x.Metadata["httpdatatype"] == expectedHttpDataResponseType);
         }
 
+//         [Fact]
+//         public async Task When_FunctionExecuted_Then_MessageSentToEventHub()
+//         {
+//             // Arrange
+//             var content = _testDocuments.ValidTimeSeries;
+//             using var whenAllEvent = await Fixture.EventHubListener
+//                 .WhenAny()
+//                 .VerifyCountAsync(1).ConfigureAwait(false);
+//             using var request = await CreateTimeSeriesHttpRequest(true, content).ConfigureAwait(false);
+//
+//             // Act
+//             await Fixture.HostManager.HttpClient.SendAsync(request).ConfigureAwait(false);
+//
+//             // Assert
+//             var allReceived = whenAllEvent.Wait(TimeSpan.FromSeconds(5));
+//             allReceived.Should().BeTrue();
+//         }
+//
+//         [Fact]
+//         public async Task When_FunctionExecuted_Then_MessageSentToEventHub_WithCreatedDateTimeSerialized()
+//         {
+//             // Arrange
+//             Fixture.EventHubListener.Reset();
+//             var jsonSerializer = new JsonSerializer();
+//             var expectedCreatedDateTime = Instant.FromUtc(2022, 12, 17, 09, 30, 47);
+//             var content = _testDocuments.ValidTimeSeries;
+//             using var whenAllEvent = await Fixture.EventHubListener
+//                 .WhenAny()
+//                 .VerifyCountAsync(1).ConfigureAwait(false);
+//             using var request = await CreateTimeSeriesHttpRequest(true, content).ConfigureAwait(false);
+//
+//             // Act
+//             await Fixture.HostManager.HttpClient.SendAsync(request).ConfigureAwait(false);
+//
+//             // Assert
+//             var allReceived = whenAllEvent.Wait(TimeSpan.FromSeconds(5));
+//             allReceived.Should().BeTrue();
+//
+//             var events = Fixture.EventHubListener.ReceivedEvents;
+//             var dto = await jsonSerializer.DeserializeAsync(events.First().BodyAsStream, typeof(TimeSeriesBundleDto))
+//                 .ConfigureAwait(false) as TimeSeriesBundleDto;
+//
+// #pragma warning disable CS8602
+//             dto.Document.CreatedDateTime.Should().BeEquivalentTo(expectedCreatedDateTime);
+// #pragma warning restore CS8602
+//         }
         private async Task<HttpRequestMessage> CreateTimeSeriesHttpRequest(bool includeJwtToken, string content)
         {
             const string requestUri = "api/" + TimeSeriesFunctionNames.TimeSeriesBundleIngestor;
