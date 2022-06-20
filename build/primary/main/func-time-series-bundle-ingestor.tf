f4fd9ba129e4f0fbeea32337d1387329f79533bf
Jonas Michelsen <jvm@energinet.dk>
2022-06-20 14:38:05 +0200

    Update func-time-series-bundle-ingestor.tf

--- build/primary/main/func-time-series-bundle-ingestor.tf
+++ build/primary/main/func-time-series-bundle-ingestor.tf
@@ -32,15 +32,9 @@ module "time_series_bundle_ingestor" {
   dotnet_framework_version                  = "6"
   use_dotnet_isolated_runtime               = true
   app_settings                              = {
-    # Region: Default Values
-    WEBSITE_ENABLE_SYNC_UPDATE_SITE                     = true
-    WEBSITE_RUN_FROM_PACKAGE                            = 1
-    WEBSITES_ENABLE_APP_SERVICE_STORAGE                 = true
-    FUNCTIONS_WORKER_RUNTIME                            = "dotnet-isolated"
     DATA_LAKE_ACCOUNT_NAME                              = data.azurerm_key_vault_secret.st_shared_data_lake_name.value
     DATA_LAKE_CONNECTION_STRING                         = data.azurerm_key_vault_secret.kvs_st_data_lake_primary_connection_string.value
     DATA_LAKE_KEY                                       = data.azurerm_key_vault_secret.kvs_st_data_lake_primary_access_key.value
-    # Shared resources logging
     REQUEST_RESPONSE_LOGGING_CONNECTION_STRING          = "@Microsoft.KeyVault(VaultName=${var.shared_resources_keyvault_name};SecretName=st-marketoplogs-primary-connection-string)",
     REQUEST_RESPONSE_LOGGING_CONTAINER_NAME             = "@Microsoft.KeyVault(VaultName=${var.shared_resources_keyvault_name};SecretName=st-marketoplogs-container-name)",
     B2C_TENANT_ID                                       = "@Microsoft.KeyVault(VaultName=${var.shared_resources_keyvault_name};SecretName=b2c-tenant-id)",
