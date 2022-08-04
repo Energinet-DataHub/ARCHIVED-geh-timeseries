1a73fe5f75d017497e80f5331d1f7880cbdc64f6
djorgensendk <32477395+djorgensendk@users.noreply.github.com>
2022-08-04 21:47:13 +0200

    Change TF authentication: Use OIDC for AzureRM and host & token for Databricks (#497)

--- build/infrastructure/main/kv-shared-resources.tf
+++ build/infrastructure/main/kv-shared-resources.tf
@@ -71,7 +71,12 @@ data "azurerm_key_vault_secret" "dbw_private_dns_resource_group_name" {
   key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
 }
 
-data "azurerm_key_vault_secret" "dbw_databricks_workspace_id" {
-  name         = "dbw-shared-workspace-id"
+data "azurerm_key_vault_secret" "dbw_databricks_workspace_url" {
+  name         = "dbw-shared-workspace-url"
+  key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
+}
+
+data "azurerm_key_vault_secret" "dbw_databricks_workspace_token" {
+  name         = "dbw-shared-workspace-token"
   key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
 }
