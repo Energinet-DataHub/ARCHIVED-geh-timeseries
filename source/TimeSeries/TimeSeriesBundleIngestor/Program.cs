007733a86073116181a984f6c86f517ac11abe67
Lasse Ringgren Nielsen <sixxth@gmail.com>
2022-08-12 15:01:51 +0200

    Update Program.cs

--- source/TimeSeries/TimeSeriesBundleIngestor/Program.cs
+++ source/TimeSeries/TimeSeriesBundleIngestor/Program.cs
@@ -30,6 +30,7 @@ using Energinet.DataHub.TimeSeries.Infrastructure.Functions;
 using Energinet.DataHub.TimeSeries.Infrastructure.Registration;
 using Energinet.DataHub.TimeSeries.TimeSeriesBundleIngestor.Monitor.Databricks;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -99,11 +100,7 @@ namespace Energinet.DataHub.TimeSeries.TimeSeriesBundleIngestor
 
             // Health check
             serviceCollection.AddScoped<IHealthCheckEndpointHandler, HealthCheckEndpointHandler>();
-            serviceCollection.AddHealthChecks()
-                .AddLiveCheck()
-                .AddAzureEventHub(name: "EventhubConnectionExists", eventHubConnectionFactory: options => new EventHubConnection(
-                    EnvironmentHelper.GetEnv("EVENT_HUB_CONNECTION_STRING"),
-                    EnvironmentHelper.GetEnv("EVENT_HUB_NAME")));
+            // TODO: LRN add healthcheck live to azureblobstorage
 
             if (EnvironmentHelper.GetEnvBool(EnvironmentSettingNames.DatabricksHealthCheckEnabled))
             {
