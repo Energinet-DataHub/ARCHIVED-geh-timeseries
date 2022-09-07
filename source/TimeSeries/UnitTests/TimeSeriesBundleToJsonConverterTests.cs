93f2b68535c7af09e22019442998836f6bbc6a43
Lasse Ringgren Nielsen <sixxth@gmail.com>
2022-09-07 11:59:45 +0200

    update nuget packages

--- source/TimeSeries/UnitTests/TimeSeriesBundleToJsonConverterTests.cs
+++ source/TimeSeries/UnitTests/TimeSeriesBundleToJsonConverterTests.cs
@@ -52,7 +52,8 @@ public class TimeSeriesBundleToJsonConverterTests
         var actual = Encoding.UTF8.GetString(stream.ToArray());
 
         // Assert
-        actual.Should().Be(expected);
+        Assert.Equal(expected, actual);
+        //actual.Should().Be(expected);
     }
 
     private TimeSeriesBundleDto CreateTestTimeSeriesBundleDto()
